3e87eb9b4578d6cd96aa44526d89913d0682d833
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-30 11:46:21 +0100

    add CryptoList.js to render data

--- src/Component/CryptoList.js
+++ src/Component/CryptoList.js
@@ -1,9 +1,52 @@
-import React from 'react';
-import CryptoItem from './CryptoItem';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-const Cryptolist = () => (
-  <>
-    <CryptoItem />
-  </>
-);
+import CryptoItem from './CryptoItem';
+import { fetchCryptoData } from '../Redux/CryptoSlice';
+import FilterInput from './FilterInput';
+
+import styles from './CryptoList.module.css';
+
+const Cryptolist = () => {
+  const dispatch = useDispatch();
+  const { cryptos, isLoading, isError } = useSelector((store) => store.crypto);
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    dispatch(fetchCryptoData());
+  }, [dispatch]);
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
+  };
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Something went wrong</p>;
+  }
+
+  // eslint-disable-next-line max-len
+  const filteredCryptos = cryptos.filter((crypto) => crypto.name.toLowerCase().includes(filter.toLowerCase()));
+
+  return (
+    <>
+
+      <form>
+        <FilterInput filter={filter} onChange={handleFilterChange} />
+      </form>
+      <div className={styles.cards}>
+        {filteredCryptos.map((crypto) => (
+        // eslint-disable-next-line react/no-array-index-key
+
+          <CryptoItem key={crypto.exchangeId} cryptoData={crypto} />
+
+        ))}
+      </div>
+    </>
+  );
+};
+
 export default Cryptolist;
