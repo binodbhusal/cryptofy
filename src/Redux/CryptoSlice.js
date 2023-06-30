94fb00119d738c011d165b58a0b1cf24dd345b5f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-06-30 11:44:42 +0100

    add redux slice cryptoSlice.js

--- src/Redux/CryptoSlice.js
+++ src/Redux/CryptoSlice.js
@@ -1,5 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
+const baseAPI = 'https://api.coincap.io/v2/exchanges/';
+export const fetchCryptoData = createAsyncThunk('cryptos/fetchCrypto', async () => {
+  try {
+    const response = await axios.get(baseAPI);
+    const { data, timestamp } = response.data;
+    const exchanges = Object.keys(data).map((exchangeId) => ({
+      exchangeId,
+      ...data[exchangeId],
+    }));
+    return {
+      exchanges, timestamp,
+    };
+  } catch (error) {
+    throw new Error('something went wrong');
+  }
+});
 const initialState = {
   cryptos: [],
   isLoading: false,
@@ -8,14 +25,21 @@ const initialState = {
 const CryptoSlice = createSlice({
   name: 'crypto',
   initialState,
-  reducers: {
-    displayCrypto: (state, action) => {
-      const cryptomsg = action.payload;
-      state.cryptos = 'Returning value';
-      return cryptomsg;
-    },
+  reducers: {},
+
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCryptoData.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+      })
+
+      .addCase(fetchCryptoData.fulfilled, (state, action) => {
+        state.cryptos = action.payload.exchanges;
+        state.isLoading = false;
+        state.isError = null;
+      });
   },
 
 });
-export const displayCrypto = CryptoSlice.actions;
 export default CryptoSlice.reducer;
